257b536eba2cfba398d1a4fd81543b5febb64017
Mardin <mardineliassi57@gmail.com>
2024-02-07 21:49:37 +0100

    read data

--- 02_Data_files/readData.js
+++ 02_Data_files/readData.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
+
 const filePath = "./me.json";
 const jsonData = fs.readFileSync(filePath, "utf-8");
-
 const parseJsonData = JSON.parse(jsonData);
-
 console.log(parseJsonData);
