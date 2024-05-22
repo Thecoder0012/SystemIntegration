487d9b4df667f447a16742e2d52a021cf49b1550
Mardin <mardineliassi57@gmail.com>
2024-05-22 20:17:16 +0200

    migrate from one db to another

--- 15_Assignments/mandatory_2/08b/prisma/seed.js
+++ 15_Assignments/mandatory_2/08b/prisma/seed.js
@@ -1,27 +1,67 @@
 // prisma/seed.js
 
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const { PrismaClient: SourcePrismaClient } = require("./generated/source");
+const { PrismaClient: DestPrismaClient } = require("./generated/dest");
+const { execSync } = require("child_process");
 
-async function main() {
-  await prisma.user.createMany({
+const sourcePrisma = new SourcePrismaClient();
+const destPrisma = new DestPrismaClient();
+
+
+async function migrateSchema() {
+  console.log("Applying existing migrations...");
+  execSync("npx prisma migrate dev --schema=./prisma/schema.prisma --name source_init");
+
+  console.log("Migrating source database schema...");
+  execSync(
+    "npx prisma migrate dev --schema=./prisma/schema_dest.prisma --name dest_init"
+  );
+
+  console.log("migration completed");
+}
+
+
+async function dropTables() {
+  console.log("Dropping tables...");
+  await sourcePrisma.$executeRaw`DROP TABLE IF EXISTS users;`;
+  await sourcePrisma.$executeRaw`DROP TABLE IF EXISTS _prisma_migrations;`;
+  await destPrisma.$executeRaw`DROP TABLE IF EXISTS user_dest;`;
+  await destPrisma.$executeRaw`DROP TABLE IF EXISTS _prisma_migrations;`;
+}
+
+
+async function seed() {
+  await sourcePrisma.user.createMany({
     data: [
       {
-        email: "alice@gmail.com",
-        password: "password123",
+        email: "testdata@example.com",
+        password: "kodeord00",
       },
       {
-        email: "bob@gmail.com",
-        password: "Bob",
-      },
-      {
-        email: "tester@gmail.com",
-        password: "test",
+        email: "tester@example.com",
+        password: "123123",
       },
     ],
   });
+  console.log("Source data seeded");
+}
 
-  console.log("Seed data created");
+async function main() {
+  await dropTables();
+  await migrateSchema();
+  await seed();
+  const users = await sourcePrisma.user.findMany();
+
+  for (const user of users) {
+    await destPrisma.dest.create({
+      data: {
+        email: user.email,
+        password: user.password,
+      },
+    });
+  }
+
+  console.log("Data migration completed");
 }
 
 main()
